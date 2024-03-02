a15fed3f40c2442adcb745dea96159b5aa95de48
xeyu134 <103041163+xeyu134@users.noreply.github.com>
2024-03-02 10:54:58 +0000

    Внес некоторые комментарии в тему 18.

--- _18_requests/_1_request_to_api.py
+++ _18_requests/_1_request_to_api.py
@@ -1,11 +1,23 @@
-import requests
+import requests  # Импортировали библиотеку requests для получения ответа из других ресурсов.
 
+# Передаем адрес получения данных, и какие параметры нам нужны
 response = requests.get('https://api.binance.com/api/v3/ticker/price', params={'symbol': 'BTCUSDT'})
 content = response.content
 print(content)
 print(type(content))
-price_object = response.json()
-price = float(price_object['price'])
+'''
+Outputs:
+b'{\n  "code": 0,\n  "msg": "Service unavailable from a restricted location according to \'b. Eligibility\' in https://www.binance.com/en/terms. Please contact customer service if you believe you received this message in error."\n}'
+<class 'bytes'>
+
+Ошибка из-за того, что сервера Replit в USA, а там нельзя делать такие запросы.
+В целом, на выходе должно быть следующее:
+'''
+price_object = response.json()  # конвертируем полученные данные в JSON.
+# Его тип - это словарь. А значит можно достать значение по ключу.
+price = float(price_object['price'])  # Получим значение, но это значение в JSON имеет тип str.
+# Поэтому лучше сразу конвертнуть в float.
+print(price)
 
 import time
 
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from _17_import.math_operations import add, subtract
-# Из папки '_17_import' импортируем модуль 'operations'
-print(add(1, 2))
-print(subtract(5, 3))
+import requests  # импортировали библиотеку requests для получения ответа из других ресурсов.
+
+url = 'https://api.binance.com/api/v3/ticker/price'
+
+# Передаем адрес получения данных, и какие параметры нам нужны
+responce = requests.get(url, params={'symbol': 'BTCUSDT'})
+
+content = responce.content
+
+print(content)
+print(type(content))
+'''
+Outputs:
+b'{\n  "code": 0,\n  "msg": "Service unavailable from a restricted location according to \'b. Eligibility\' in https://www.binance.com/en/terms. Please contact customer service if you believe you received this message in error."\n}'
+<class 'bytes'>
+
+Ошибка из-за того, что сервера Replit в USA, а там нельзя делать такие запросы.
+В целом, на выходе должно быть следующее:
+
+'''
+
+content_2 = responce.json()  # конвертируем полученные данные в JSON.
+# Его тип - это словарь. А значит можно достать значение по ключу.
+price = content_2['price']  # получим значение, но это значение в JSON имеет тип str.
+price = float(content_2['price'])  # Поэтому лучше сразу конвертнуть в float.
+
+print(price)
+
